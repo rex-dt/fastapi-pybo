14bf981079500267beaca10bfb49bcc863adf488
rexlim <rex.lim@digitaltrinity.co.kr>
2023-08-08 18:03:29 +0900

    파이보 프로젝트 최초 커밋

---
+++ frontend/src/components/Error.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let error  // 전달받은 오류
+</script>
+
+{#if typeof error.detail === 'string'}
+<div class="alert alert-danger" role="alert">
+    <div>
+        {error.detail}
+    </div>
+</div>
+    <!-- <ul>
+        <li>{error.detail}</li>
+    </ul> -->
+{:else if typeof error.detail === 'object' && error.detail.length > 0 }
+<div class="alert alert-danger" role="alert">
+    {#each error.detail as err, i}
+    <div>
+        <strong>{err.loc[1]}</strong> : {err.msg}
+    </div>
+    {/each}
+</div>
+    <!-- <ul>
+        {#each error.detail as err, i}
+        <li>
+            <strong>{err.loc[1]}</strong> : {err.msg}
+        </li>
+        {/each}
+    </ul> -->
+{/if}
